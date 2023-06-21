67f0b947c4bb71037ecefc8d81d74b5b9fdf0c8e
mprigent <77842628+mprigent@users.noreply.github.com>
2023-06-21 20:36:10 +0200

    last version - ex00 + ex01

---
+++ cpp_01/ATarget.cpp
@@ -0,0 +1,25 @@
+#include "ATarget.hpp"
+
+ATarget::ATarget() {}
+
+ATarget::ATarget(const ATarget &w) : _type(w._type) {}
+
+ATarget::ATarget(const std::string &type) : _type(type) {}
+
+ATarget::~ATarget() {}
+
+ATarget	&ATarget::operator=(const ATarget &w)
+{
+	this->_type = w.getType();
+	return (*this);
+}
+
+const std::string	&ATarget::getType() const 
+{
+	return this->_type;
+}
+
+void	ATarget::getHitBySpell(const ASpell &spell) const 
+{
+	std::cout << this->_type << " has been " << spell.getEffects() << "!" << std::endl;
+}
---
+++ cpp_01/Fwoosh.hpp
@@ -0,0 +1,18 @@
+#ifndef FWOOSH_HPP
+# define FWOOSH_HPP
+
+#include "ASpell.hpp"
+
+class Fwoosh;
+
+class Fwoosh : public ASpell {
+
+	public :
+
+		Fwoosh();
+		virtual ~Fwoosh();
+
+		virtual ASpell *clone() const;
+};
+
+#endif
